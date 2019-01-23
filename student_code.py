49fd2b59d18433fbb477454f510abd697e94b078
Ava Robinson <avamrobinson@gmail.com>
2019-01-22 22:34:43 -0600

    assignment-1-kb-basics-avarobinson

--- student_code.py
+++ student_code.py
@@ -18,14 +18,40 @@ class KnowledgeBase(object):
         return string
 
     def kb_assert(self, fact):
+        #if fact is a fact
+        if isinstance(fact, Fact):
+            #see if fact is already in facts list
+            for f in self.facts:
+                #if fact is already in facts KB then dont add
+                if f == fact:
+                    return
+            self.facts.append(fact)
+        print("Asserting {!r}".format(fact))
+        """elif fact.name == 'rule':
+            #See if that rule is already in rules list
+            for r in self.rules:
+                #if fact already in rules KB then dont add
+                if r == fact:
+                    return
+            self.rules.append(fact)
+            fact.asserted = True;"""
         """Assert a fact or rule into the KB
 
         Args:
             fact (Fact or Rule): Fact or Rule we're asserting in the format produced by read.py
         """
-        print("Asserting {!r}".format(fact))
         
     def kb_ask(self, fact):
+        found_bindings = ListOfBindings()
+        for f in self.facts:
+            matches = match(fact.statement, f.statement)
+            if matches != False:
+                found_bindings.add_bindings(matches, [f])
+        print("Asking {!r}".format(fact))
+        if len(found_bindings) == 0: #if not matches found for the fact --> return False
+            return False
+        else:
+            return found_bindings
         """Ask if a fact is in the KB
 
         Args:
@@ -34,4 +60,3 @@ class KnowledgeBase(object):
         Returns:
             ListOfBindings|False - ListOfBindings if result found, False otherwise
         """
-        print("Asking {!r}".format(fact))
